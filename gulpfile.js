cfb5a90ba99fc593dce4ce1b283210a3ab006711
xudafeng <xudafeng@126.com>
2017-02-01 00:12:05 +0800

    add webpack

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var babelify = require('babelify');
+var stringify = require('stringify');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('js', function() {
+  return browserify({
+    debug: true,
+    ignore: [],
+    bundleExternal: false
+  })
+  .transform(stringify(['.html']))
+  .transform(babelify.configure({ extensions: ['.js', '.jsx', '.ts', '.tsx'] }))
+  .add('./lib/assets/index.js')
+  .bundle()
+  .pipe(source('./index.js'))
+  .pipe(buffer())
+  .pipe(gulp.dest('./lib/dist'));
+});
+
+gulp.task('less', function() {
+  return gulp
+    .src('./lib/assets/index.less')
+    .pipe(less())
+    .pipe(gulp.dest('./lib/dist'));
+});
+
+gulp.task('watch', ['js', 'less'], function() {
+  gulp.watch(['lib/assets/*.js', 'lib/assets/**/*.js'], ['js']);
+  gulp.watch(['lib/assets/*.less', 'lib/assets/**/*.less'], ['less']);
+});
+
+gulp.task('default', ['js', 'less']);
